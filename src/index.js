e01134ef5ea0057e95a327acbb84a3011c60546a
MaSkA6293 <MaSkA6293@yandex.ru>
2021-02-01 22:01:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  for (let i = 0; i < bracketsConfig.length * 2; i++) {
+    bracketsConfig.forEach(element => {
+      let regExpr = element.join('')
+      str = checker(str, regExpr)
+    });
+  }
+
+  function checker(str, reg) {
+    let replace = str
+    for (let i = 0; i < str.length / 2; i++) {
+      replace = replace.replace(reg, '')
+    }
+    return replace
+  }
+  return str ? false : true
 }
